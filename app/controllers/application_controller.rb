8c56d36b6bda8132822dcabe4bb2c82df40abf73
Sylphux <masip.pablo@gmail.com>
2025-11-01 11:07:44 +0100

    add login elements to nav, redirect to home when not logged

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login, except: [:home] #redirige vers home quand on est pas connecté (sauf depuis la page home elle même)
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+  def require_login
+    unless current_user
+      flash[:danger] = "Please log in"
+      redirect_to home_path
+    end
+  end
+
 end
