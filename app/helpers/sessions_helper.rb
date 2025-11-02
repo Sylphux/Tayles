9e65a95475f2f6746955f8e7f0bb92578f1bbf95
Sylphux <masip.pablo@gmail.com>
2025-11-02 23:08:25 +0100

    corrected small bug, commented session helpers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,12 @@
 module SessionsHelper
-    def shorten_text(s, n)
+    def shorten_text(s, n) #shorten a string / text. s is the input string, n is the desired length
         if s.length > n
             return (s[0..n-3] + "...")
         end
         return s
     end
 
-    def node_belongs_to_user?(n)
+    def node_belongs_to_user?(n) #verifies if node belongs to user
         for u in n.world.users
             if u == current_user
                 return true
@@ -15,7 +15,7 @@ module SessionsHelper
         return false
     end
 
-    def node_is_known?(n)
+    def node_is_known?(n) #verifies if node is known by user (so user is an explorer)
         for u in n.users
             if u == current_user
                 return true
@@ -24,7 +24,7 @@ module SessionsHelper
         return false
     end
 
-    def node_discovered_on(n)
+    def node_discovered_on(n) #gives the date node was revealed to current user
         for x in n.known_nodes
             if x.user == current_user
                 return x.created_at
@@ -32,7 +32,7 @@ module SessionsHelper
         end
     end
 
-    def user_team(n)
+    def user_team(n) #returns the team on which user is exploring the world
         for t in n.world.teams do
             for u in t.users do
                 if u == current_user
